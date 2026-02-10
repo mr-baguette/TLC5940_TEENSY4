f7909aab0048582d8963c234763d1f2ebad55878
mr-baguette <vanlangendonckjoren@gmail.com>
2026-02-10 21:08:50 +0100

    (Gemini) Refactor TLC5940Teensy4 header and improve structure

--- src/TLC5940_Teensy4.h
+++ src/TLC5940_Teensy4.h
@@ -2,67 +2,56 @@
 #define TLC5940_TEENSY4_H
 
 #include <Arduino.h>
-#include "TLC5940_config.h"
+#include "TLC5940_Config.h"
 
 class TLC5940Teensy4 {
 public:
-  static constexpr uint16_t kChannels = TLC5940_NUM_CHIPS * 16;
-
-  enum class XerrType : uint8_t {
-    kNone,
-    kLedOpen,
-    kThermal,
-  };
-
-  TLC5940Teensy4();
-
-  void begin();
-  void set(uint16_t channel, uint16_t value);
-  void setAll(uint16_t value);
-  void update();
+    enum class XerrType { kNone, kThermal, kLedOpen };
+    TLC5940Teensy4();
+    void begin();
+    void set(uint16_t channel, uint16_t value);
+    void setAll(uint16_t value);
+    void update();
 #if TLC5940_VPRG_ENABLED
-  void setDc(uint16_t channel, uint8_t value);
-  void setAllDc(uint8_t value);
-  void updateDc();
+    void setDc(uint16_t channel, uint8_t value);
+    void setAllDc(uint8_t value);
+    void updateDc();
 #endif
-
-  void pollXerr();
-  bool hasXerr() const;
-  XerrType xerrType() const;
+    void pollXerr();
+    bool hasXerr() const;
+    XerrType xerrType() const;
 
 private:
-    volatile bool pendingLatch_ = false; 
-    
-    // Internal hardware setup
-    void configureTimers_(); 
-    
-    // Statics for the hardware interrupt
-    static TLC5940Teensy4* instance_;
-    static void onFrameSyncIsr_(); 
+    static const uint16_t kChannels = 16 * TLC5940_NUM_CHIPS;
+    uint16_t grayscale_[kChannels];
+#if TLC5940_VPRG_ENABLED
+    uint8_t dotCorrection_[kChannels];
+#endif
 
-    // Keep these as empty stubs for now so you don't have to 
-    // hunt down and delete calls to them elsewhere.
+    // MUST be volatile for ISR visibility
+    volatile bool pendingLatch_ = false;
+    uint32_t lastBlankPulseMicros_ = 0;
+    XerrType lastXerrType_ = XerrType::kNone;
+
+    void configureTimers_();
+    void setControlMode_(bool dcMode);
+    void writeGrayscaleData_();
+    void writeBitBangByte_(uint8_t value);
+    void pulseBlank_();
+    void updateXerrType_(bool xerrLow, bool blankPulseActive);
+
+#if TLC5940_VPRG_ENABLED
+    void writeDotCorrectionData_();
+#endif
+
+    // Hardware Interface
+    static TLC5940Teensy4* instance_;
+    static void onFrameSyncIsr_();
+
+    // Codex Stubs
     void configureGsclk_() {}
     void configureFrameSyncIsr_() {}
     void handleFrameSync_() {}
-
-  uint16_t grayscale_[kChannels];
-#if TLC5940_VPRG_ENABLED
-  uint8_t dotCorrection_[kChannels];
-#endif
-  uint32_t lastBlankPulseMicros_ = 0;
-  XerrType lastXerrType_ = XerrType::kNone;
-  volatile bool pendingLatch_ = false;
-
-  void writeGrayscaleData_();
-#if TLC5940_VPRG_ENABLED
-  void writeDotCorrectionData_();
-#endif
-  void latch_();
-  void setControlMode_(bool dcMode);
-  void writeBitBangByte_(uint8_t value);
-  void pulseBlank_();
-  void updateXerrType_(bool xerrLow, bool blankPulseActive);
 };
 
-#endif // TLC5940_TEENSY4_H
+#endif
